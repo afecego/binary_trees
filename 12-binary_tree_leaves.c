d217387a027d8c9ba7a9fc9d67610cea4f3db6e8
afecego <afecego@gmail.com>
2021-12-01 19:51:26 -0500

    COunt nodos child

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - function that counts the leaves in a binary tree
+ * binary_tree_leaves - function that counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to count the number of leaves.
  * Return: If tree is NULL, your function must return 0
  */
